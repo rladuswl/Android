1dc6b45293bc7e95ec742c5861b77f281f3bdb99
khkkg <khkkgon0118@gmail.com>
2022-03-20 14:38:27 +0900

    Button, TextView, EditText 속성 사용하기

---
+++ Self4_2/app/src/main/java/com/example/self4_2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.self4_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 앱 아이콘 출력
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+
+        //button1.setBackgroundColor(Color.WHITE);
+    }
+}
