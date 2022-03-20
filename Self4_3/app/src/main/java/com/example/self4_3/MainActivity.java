1b54b51b3e26859f3afef4818c691ec8a60f5337
khkkg <khkkgon0118@gmail.com>
2022-03-20 18:22:21 +0900

    초간단 계산기 앱(기능 추가)

---
+++ Self4_3/app/src/main/java/com/example/self4_3/MainActivity.java
@@ -0,0 +1,148 @@
+package com.example.self4_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+// 기능이 추가된 초간단 계산기 앱
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv, btnRem;
+    TextView textResult;
+    String num1, num2;
+    Double result; // 실수값으로 계산
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 앱 아이콘 출력
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+
+        // 계산기 기능 시작
+        setTitle("초간단 계산기(수정)");
+
+        edit1 = (EditText) findViewById(R.id.Edit1);
+        edit2 = (EditText) findViewById(R.id.Edit2);
+
+        btnAdd = (Button) findViewById(R.id.BtnAdd);
+        btnSub = (Button) findViewById(R.id.BtnSub);
+        btnMul = (Button) findViewById(R.id.BtnMul);
+        btnDiv = (Button) findViewById(R.id.BtnDiv);
+        btnRem = (Button) findViewById(R.id.BtnRem);
+
+        textResult = (TextView) findViewById(R.id.TextResult);
+
+        // 더하기
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                // num1이나 num2가 비어 있다면
+                if (num1.trim().equals("") || num2.trim().equals("")) {
+                    Toast.makeText(getApplicationContext(), "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+                    result = Double.parseDouble(num1) + Double.parseDouble(num2);
+                    textResult.setText("계산 결과 : " + result.toString());
+                }
+            }
+        });
+
+        // 빼기
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                // num1이나 num2가 비어 있다면
+                if (num1.trim().equals("") || num2.trim().equals("")) {
+                    Toast.makeText(getApplicationContext(), "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+                    result = Double.parseDouble(num1) - Double.parseDouble(num2);
+                    textResult.setText("계산 결과 : " + result.toString());
+                }
+            }
+        });
+
+        // 곱하기
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                // num1이나 num2가 비어 있다면
+                if (num1.trim().equals("") || num2.trim().equals("")) {
+                    Toast.makeText(getApplicationContext(), "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+                    result = Double.parseDouble(num1) * Double.parseDouble(num2);
+                    textResult.setText("계산 결과 : " + result.toString());
+                }
+            }
+        });
+
+        // 나누기
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                // num1이나 num2가 비어 있다면
+                if (num1.trim().equals("") || num2.trim().equals("")) {
+                    Toast.makeText(getApplicationContext(), "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+                    // num2가 0이면 나누지 않는다.
+                    if (num2.trim().equals("0")) {
+                        Toast.makeText(getApplicationContext(), "0으로 나눌 수 없습니다.", Toast.LENGTH_SHORT)
+                                .show();
+                    } else {
+                        result = Double.parseDouble(num1) / Double.parseDouble(num2);
+                        result = (int) (result * 10) / 10.0; // 소수점 아래 1자리까지만 출력
+                        textResult.setText("계산 결과 : " + result.toString(result));
+                    }
+                }
+            }
+        });
+
+        // 나머지 값
+        btnRem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                // num1이나 num2가 비어 있다면
+                if (num1.trim().equals("") || num2.trim().equals("")) {
+                    Toast.makeText(getApplicationContext(), "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+                    // num2가 0이면 나누지 않는다.
+                    if (num2.trim().equals("0")) {
+                        Toast.makeText(getApplicationContext(), "0으로 나머지 값을 구할 수 없습니다.", Toast.LENGTH_SHORT)
+                                .show();
+                    } else {
+                        result = Double.parseDouble(num1) % Double.parseDouble(num2);
+                        textResult.setText("계산 결과 : " + result.toString());
+                    }
+                }
+            }
+        });
+    }
+}
