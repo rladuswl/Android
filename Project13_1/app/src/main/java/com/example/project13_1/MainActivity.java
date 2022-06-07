65f40ae9b623f535b15463f7b62fbe26580e0aae
khkkg <khkkgon0118@gmail.com>
2022-06-08 00:11:00 +0900

    MP3 플레이어 앱

--- Project13_1/app/src/main/java/com/example/project13_1/MainActivity.java
+++ Project13_1/app/src/main/java/com/example/project13_1/MainActivity.java
@@ -7,30 +7,34 @@ import android.Manifest;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listViewMP3;
     Button btnPlay, btnStop, btnPause;
-    TextView tvMP3;
-    ProgressBar pbMP3;
+    TextView tvMP3, tvTime;
+    SeekBar pbMP3;
 
     ArrayList<String> mp3List;
     String selectedMP3;
 
     String mp3path = Environment.getExternalStorageDirectory().getPath() + "/";
     MediaPlayer mPlayer;
+    boolean PAUSED = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,7 +72,56 @@ public class MainActivity extends AppCompatActivity {
         btnStop = (Button) findViewById(R.id.btnStop);
         btnPause = (Button) findViewById(R.id.btnPause);
         tvMP3 = (TextView) findViewById(R.id.tvMP3);
-        pbMP3 = (ProgressBar) findViewById(R.id.pbMP3);
+        pbMP3 = (SeekBar) findViewById(R.id.pbMP3);
+        tvTime = (TextView) findViewById(R.id.tvTime);
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (PAUSED == false) {
+                    mPlayer.pause();
+                    btnPause.setText("이어듣기");
+                    PAUSED = true;
+                    //pbMP3.setVisibility(View.INVISIBLE);
+                    new Thread() {
+                        SimpleDateFormat timeFormat = new SimpleDateFormat("mm:ss");
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    tvTime.setText("진행 시간 : " + timeFormat.format(mPlayer.getCurrentPosition()));
+                                }
+                            });
+                        }
+                    }.start();
+
+                } else {
+                    mPlayer.start();
+                    PAUSED = false;
+                    btnPause.setText("일시정지");
+                    //pbMP3.setVisibility(View.VISIBLE);
+                    new Thread() {
+                        SimpleDateFormat timeFormat = new SimpleDateFormat("mm:ss");
+                        @Override
+                        public void run() {
+                            if (mPlayer == null)
+                                return;
+                            while (mPlayer.isPlaying()) {
+                                runOnUiThread(new Runnable() {
+                                    public void run() {
+                                        pbMP3.setProgress(mPlayer.getCurrentPosition());
+                                        tvTime.setText("진행 시간 : " + timeFormat.format(mPlayer.getCurrentPosition()));
+                                    }
+                                });
+                                SystemClock.sleep(200);
+                            }
+                        }
+                    }.start();
+                }
+
+            }
+        });
 
         btnPlay.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -79,8 +132,27 @@ public class MainActivity extends AppCompatActivity {
                     mPlayer.start();
                     btnPlay.setClickable(false);
                     btnStop.setClickable(true);
+                    //pbMP3.setVisibility(View.VISIBLE);
                     tvMP3.setText("실행중인 음악 :  " + selectedMP3);
-                    pbMP3.setVisibility(View.VISIBLE);
+                    new Thread() {
+                        SimpleDateFormat timeFormat = new SimpleDateFormat("mm:ss");
+                        @Override
+                        public void run() {
+                            if (mPlayer == null)
+                                return;
+                            pbMP3.setMax(mPlayer.getDuration());
+                            while (mPlayer.isPlaying()) {
+                                runOnUiThread(new Runnable() {
+                                    public void run() {
+                                        pbMP3.setProgress(mPlayer.getCurrentPosition());
+                                        tvTime.setText("진행 시간 : " + timeFormat.format(mPlayer.getCurrentPosition()));
+                                    }
+                                });
+                                SystemClock.sleep(200);
+                            }
+                        }
+                    }.start();
+
                 } catch (IOException e) {
                 }
             }
@@ -93,9 +165,41 @@ public class MainActivity extends AppCompatActivity {
                 btnPlay.setClickable(true);
                 btnStop.setClickable(false);
                 tvMP3.setText("실행중인 음악 :  ");
-                pbMP3.setVisibility(View.INVISIBLE);
+                //pbMP3.setVisibility(View.INVISIBLE);
+                new Thread() {
+                    SimpleDateFormat timeFormat = new SimpleDateFormat("mm:ss");
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                pbMP3.setProgress(0);
+                                tvTime.setText("진행 시간 : ");
+                            }
+                        });
+                    }
+                }.start();
             }
         });
         btnStop.setClickable(false);
+
+        pbMP3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (b) {
+                    mPlayer.seekTo(i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
