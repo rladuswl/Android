cf4b85146f236a975eb0930f03c4e84fec924760
khkkg <khkkgon0118@gmail.com>
2022-06-10 00:04:18 +0900

    그리드뷰 영화 포스터

---
+++ Project11_2/app/src/main/java/com/example/project11_2/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.project11_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Gallery gallery = (Gallery) findViewById(R.id.gallery);
+        MyGalleryAdapter galAdapter = new MyGalleryAdapter(this);
+        gallery.setAdapter(galAdapter);
+    }
+
+    public class MyGalleryAdapter extends BaseAdapter {
+        Context context;
+
+        Integer[] posterID = { R.drawable.mov11, R.drawable.mov12,
+                R.drawable.mov13, R.drawable.mov14, R.drawable.mov15,
+                R.drawable.mov16, R.drawable.mov17, R.drawable.mov18,
+                R.drawable.mov19, R.drawable.mov20 };
+
+        String[] posterTitle = { "레옹", "주먹왕 랄프2", "타짜","걸캅스", "도굴", "어벤저스",
+                "엑시트", "캡틴 마블", "봉오동 전투", "분노의 질주"};
+
+        public MyGalleryAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return posterID.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ImageView imageView = new ImageView(context);
+            imageView.setLayoutParams(new Gallery.LayoutParams(200, 300));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setPadding(5, 5, 5, 5);
+
+            imageView.setImageResource(posterID[i]);
+
+            final int pos = i;
+            imageView.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    ImageView ivPoster = (ImageView) findViewById(R.id.ivPoster);
+                    ivPoster.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                    ivPoster.setImageResource(posterID[pos]);
+                    Toast.makeText(getApplicationContext(), posterTitle[pos], Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            });
+            return imageView;
+        }
+    }
+}
